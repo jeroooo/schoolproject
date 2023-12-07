55c192153f2691a1bf7a43c89a0fdf54cde113d0
jeroooo <64100349+jeroooo@users.noreply.github.com>
2023-12-07 09:31:13 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
 from tkinter import*
 from requests import*
 from json import*
+from urllib.request import urlopen
+
 # https://random-d.uk/api api za slike pataka
 p=Tk()
 b=Button(p,'Stisni za patku')
