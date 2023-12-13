d2490558b526fd90489f07c876a7b24b32cd7e29
jeroooo <64100349+jeroooo@users.noreply.github.com>
2023-12-13 22:46:19 +0100

    yes

---
+++ aiapi.py
@@ -0,0 +1,29 @@
+from tkinter import*
+from PIL import Image, ImageTk
+import requests
+from playsound import*
+
+def get_duck_image():
+
+    api_url = "https://source.unsplash.com/featured/?duck"
+
+    response = requests.get(api_url)
+    
+    image = Image.open(requests.get(api_url, stream=True).raw)
+    photo = ImageTk.PhotoImage(image)
+    
+    label.config(image=photo)
+    label.image = photo
+    b.config(text='STISNI ZA JOŠ JEDNU')
+    playsound('1.mp3')
+
+p = Tk()
+p.title("Patke patke patkice")
+
+b =Button(p, text="STISNI ZA SLIKU PATKICA", command=get_duck_image)
+b.pack()
+
+label = Label(p)
+label.pack()
+
+mainloop()
